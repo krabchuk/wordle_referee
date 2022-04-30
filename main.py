53f9e8da299aee6aa8a3d58de3b89b1e3e9ce7d5
krabchuk <krabchuk@gmail.com>
2022-04-30 18:15:59 +0300

    FIX: New constant in start

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import tokens
 
 @log_message
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Welcome to WordleBot! v0")
+    update.message.reply_text("Welcome to WordleBot! v1")
 
 
 def main():
