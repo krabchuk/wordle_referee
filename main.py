6905ef0596dea03edefd8ec9841d6912ca136460
krabchuk <krabchuk@gmail.com>
2022-04-30 15:13:57 +0300

    NEW: Having more fun with docker env

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from telegram import KeyboardButton, ReplyKeyboardMarkup, Update
-from telegram.ext import *
-from utils import *
-
-import tokens
-
-
-@log_message
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Welcome to WordleBot! v0")
+# from telegram import KeyboardButton, ReplyKeyboardMarkup, Update
+# from telegram.ext import *
+# from utils import *
+#
+# import tokens
+#
+#
+# @log_message
+# def start(update: Update, context: CallbackContext):
+#     update.message.reply_text("Welcome to WordleBot! v0")
 
 
 def main():
 
     import os
-    print(os.environ)
+    envs = []
+    for key, value in os.environ.items():
+        envs.append((key, value))
+    envs.sort(key=lambda x: x[0])
+    for key, value in envs:
+        print(f'{key}: "{value}"')
     return
 
     updater = Updater(token=tokens.token)
